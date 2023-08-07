22d7b126560cc0ef5cf12ac257717d7eb0c5a632
Powerchip15 <130801086+Powerchip15@users.noreply.github.com>
2023-08-06 19:15:22 -0600

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -15,7 +15,7 @@ public func configure(_ app: Application) async throws {
         username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
         password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
         database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
+        tls: .none)
     ), as: .psql)
 
     app.migrations.add(CreateTests())
